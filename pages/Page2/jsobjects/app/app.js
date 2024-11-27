6d1ac258c82314a45bae1a666ee5178ff45453f1
mashiourcse <mashiourcse>
2024-11-27 16:38:57 +0000

    added persistant store

--- pages/Page2/jsobjects/app/app.js
+++ pages/Page2/jsobjects/app/app.js
@@ -1,15 +1,17 @@
 export default {
 	init () {
-		data.todos = data.dummyTodos;
+		data.todos = appsmith.store.todos || data.dummyTodos;
 	},
 	updateTodo( id, update){
-		data.todos[id] = {...data.todos[id], ...update}
+		data.todos[id] = {...data.todos[id], ...update};
+		storeValue('todos', data.todos);
 	},
 	addTodo( title){
 		this.updateTodo(data.todos.length, {title:title, completed: false})
 	},
 	deleteTodo(id){
 		data.todos.splice(id, 1);
+		storeValue('todos', data.todos);
 	},
 	editTodo(id, title){
 		this.updateTodo(id, {title});
